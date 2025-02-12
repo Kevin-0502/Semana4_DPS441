3f8d16573fa66b590cdd848a2856c63f2dd3e678
Kevin Jiménez <kmjh140217@gmail.com>
2025-02-12 12:20:56 -0600

    'recursos_semana_4'

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ListaTareas from './ListaTareas';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ListaTareas />
+    </div>
+  );
+}
+
+export default App;
